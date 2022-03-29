c05f376179fcc115a1c1a4d255354398f593fe3b
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-29 16:11:08 -0500

    Update GameOverManager.cs

--- Codes/GameOverManager.cs
+++ Codes/GameOverManager.cs
@@ -7,84 +7,93 @@ using UnityEngine.UI;
 public class GameOverManager : MonoBehaviour
 {
     public GameObject Database;
-    
+
     public GameObject BestScoreTag;
     public GameObject PointS;
     public GameObject PointA;
     public GameObject PointB;
     public GameObject PointC;
     public GameObject PointD;
-    
+
     public GameObject BestScore;
     public GameObject NewScore;
-    
+
     float BS = 0;
     float BSU = 0;
     float SC = 0;
-    
+
     Text BestST;
     Text NewST;
-    
+
     void Start()
     {
         if (Database.GetComponent<Database>().ReadDataS("GameData", "Data", "status", 1) == "done")
         {
             BS = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 4);
-            SC = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 1); 
-            
+            SC = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 1);
+
             NewST = NewScore.GetComponent<Text>(); //get score text
             NewST.text = string.Format("{0:0}", SC); //score text format
-            
-            if (SC > BS) {
-                Database.GetComponent<Databse>().UpdateData("GameData", "Data", 4, "score", SC);
+
+            if (SC > BS)
+            {
+                Database.GetComponent<Database>().UpdateData("GameData", "Data", 4, "score", SC.ToString());
                 BSU = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 4);
-                BestST = BestScore.GetComponent<Text>(); 
-                BestST.text = string.Format("{0:0}", BSU);
-            } else {
                 BestST = BestScore.GetComponent<Text>();
                 BestST.text = string.Format("{0:0}", BSU);
             }
-         }
-         
-        if (Database.GetComponent<Database>().ReadDataS("GameData", "Data", "status", 2) == "done")
-        {
-            BS = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 5);
-            SC = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 2); 
-            
-            NewST = NewScore.GetComponent<Text>(); //get score text
-            NewST.text = string.Format("{0:0}", SC); //score text format
-            
-            if (SC > BS) {
-                Database.GetComponent<Databse>().UpdateData("GameData", "Data", 5, "score", SC);
-                BSU = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 5);
-                BestST = BestScore.GetComponent<Text>(); 
-                BestST.text = string.Format("{0:0}", BSU);
-            } else {
+            else
+            {
                 BestST = BestScore.GetComponent<Text>();
                 BestST.text = string.Format("{0:0}", BSU);
             }
         }
-         
-        if (Database.GetComponent<Database>().ReadDataS("GameData", "Data", "status", 3) == "done")
+
+        if (Database.GetComponent<Database>().ReadDataS("GameData", "Data", "status", 2) == "done")
         {
-            BS = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 6);
-            SC = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 3); 
-            
+            BS = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 5);
+            SC = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 2);
+
             NewST = NewScore.GetComponent<Text>(); //get score text
             NewST.text = string.Format("{0:0}", SC); //score text format
-            
-            if (SC > BS) {
-                Database.GetComponent<Databse>().UpdateData("GameData", "Data", 6, "score", SC);
-                BSU = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 6);
-                BestST = BestScore.GetComponent<Text>(); 
-                BestST.text = string.Format("{0:0}", BSU);
-            } else {
+
+            if (SC > BS)
+            {
+                Database.GetComponent<Database>().UpdateData("GameData", "Data", 5, "score", SC.ToString());
+                BSU = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 5);
                 BestST = BestScore.GetComponent<Text>();
                 BestST.text = string.Format("{0:0}", BSU);
             }
-         }
+            else
+            {
+                BestST = BestScore.GetComponent<Text>();
+                BestST.text = string.Format("{0:0}", BSU);
+            }
+        }
+
+        if (Database.GetComponent<Database>().ReadDataS("GameData", "Data", "status", 3) == "done")
+        {
+            BS = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 6);
+            SC = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 3);
+
+            NewST = NewScore.GetComponent<Text>(); //get score text
+            NewST.text = string.Format("{0:0}", SC); //score text format
+
+            if (SC > BS)
+            {
+                Database.GetComponent<Database>().UpdateData("GameData", "Data", 6, "score", SC.ToString());
+                BSU = Database.GetComponent<Database>().ReadDataI("GameData", "Data", "score", 6);
+                BestST = BestScore.GetComponent<Text>();
+                BestST.text = string.Format("{0:0}", BSU);
+            }
+            else
+            {
+                BestST = BestScore.GetComponent<Text>();
+                BestST.text = string.Format("{0:0}", BSU);
+            }
+        }
     }
-    
+
     void Reset()
     {
         if (Database.GetComponent<Database>().ReadDataS("GameData", "Data", "status", 1) == "done")
@@ -100,13 +109,13 @@ public class GameOverManager : MonoBehaviour
             Database.GetComponent<Database>().UpdateData("GameData", "Data", 3, "score, status", "0, 'playing'");
         }
     }
-    
+
     public void Restart()
     {
         Reset();
         SceneManager.LoadScene("LoadingScene");
     }
-    
+
     public void ReturnToLobby()
     {
         Reset();
