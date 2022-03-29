a84765c9057568122c45f4b0cb02ed7235ee93f4
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-28 20:29:26 -0500

    Update TitleManager.cs

--- Codes/TitleManager.cs
+++ Codes/TitleManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class TitleManager : MonoBehaviour
@@ -13,11 +14,26 @@ public class TitleManager : MonoBehaviour
     public GameObject Description; //info des area
     public GameObject DReturn_Key; //return to title
 
+    public Text Title;
+    public string TitleText = "FORBIDDEN FOREST";
+    public float TextSpeed = 0.2f;
+
     void Start() //when start
-    {
+    { 
         BackGround.SetActive(true); //background on
         ButtonArea.SetActive(true); //button area on
         InfoArea.SetActive(false); //turn off info(tutorial) area
+        StartCoroutine(Typing(Title, TitleText, TextSpeed));
+    }
+
+    IEnumerator Typing(Text textBox, string msg, float speed)
+    {
+        for (int i = 0; i < msg.Length; i++)
+        {
+            textBox.text = msg.Substring(0, i + 1);
+            yield return new WaitForSeconds(speed);
+        }
+
     }
 
     public void StartK() //if press start
