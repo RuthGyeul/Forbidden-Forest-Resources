bb3e263a37e5cac5fe3d27a38a75ad7f813676f6
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-28 22:00:38 -0500

    Update PlayerControl.cs

--- Codes/PlayerControl.cs
+++ Codes/PlayerControl.cs
@@ -9,30 +9,13 @@ public class PlayerControl : MonoBehaviour
     Vector2 moveLimit = new Vector2(7.0f, 0); //player position limit
     Vector3 playerPosition; //player position
     public GameObject GameManager; //get gamemanager
-    public GameObject Option;
     public float moveSpeed = 8.0f; //move speed
     GameManager GM;
 
     void Update()
     {
         GM = GameManager.GetComponent<GameManager>(); //get gamemanager component
-
-        if (GM.GameOnGoing) //if game is not paused let the key move
-        {
-            ActiveKey(); //move
-        }
-
-        if (Input.GetKeyDown(KeyCode.P)) //pause game
-        {
-            GM.GameOnGoing = false;
-            Option.SetActive(true);
-        }
-
-        if (Input.GetKeyDown(KeyCode.T)) //resume game
-        {
-            Option.SetActive(false);
-            GameStartCount();
-        }
+        ActiveKey(); //move
     }
 
     public void ActiveKey()
@@ -41,29 +24,26 @@ public class PlayerControl : MonoBehaviour
         rend = GetComponent<SpriteRenderer>(); //get renerer
         playerPosition = ClampPosition(this.transform.position); //get player position & position limit
 
-        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        if (GM.GameOnGoing) //if game is not paused let the key move
         {
-            rend.flipX = true;
-            playerPosition += Vector3.left * moveSpeed * Time.deltaTime;
-            rigid.MovePosition(playerPosition);
-        }
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                rend.flipX = true;
+                playerPosition += Vector3.left * moveSpeed * Time.deltaTime;
+                rigid.MovePosition(playerPosition);
+            }
 
-        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
-        {
-            rend.flipX = false;
-            playerPosition += Vector3.right * moveSpeed * Time.deltaTime;
-            rigid.MovePosition(playerPosition);
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                rend.flipX = false;
+                playerPosition += Vector3.right * moveSpeed * Time.deltaTime;
+                rigid.MovePosition(playerPosition);
+            }
         }
     }
 
     public Vector3 ClampPosition(Vector3 position)
     {
         return new Vector3(Mathf.Clamp(position.x, -moveLimit.x, moveLimit.x), -3f, 0); //player position always set on x = moveLimit, y = -3, z = 0
-    }
-
-    public void GameStartCount()
-    {
-        //add count down 3
-        GM.GameOnGoing = true;
     }
 }
