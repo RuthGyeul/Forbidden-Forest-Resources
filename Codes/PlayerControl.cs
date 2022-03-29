796f8ef509c2366217daa465fdb3c0e302e1a8bf
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-28 20:56:05 -0500

    Update PlayerControl.cs

--- Codes/PlayerControl.cs
+++ Codes/PlayerControl.cs
@@ -8,11 +8,31 @@ public class PlayerControl : MonoBehaviour
     SpriteRenderer rend; //get renderer
     Vector2 moveLimit = new Vector2(7.0f, 0); //player position limit
     Vector3 playerPosition; //player position
+    public GameObject GameManager; //get gamemanager
+    public GameObject Option;
     public float moveSpeed = 8.0f; //move speed
-   
+    GameManager GM;
+
     void Update()
     {
-        ActiveKey(); //active key
+        GM = GameManager.GetComponent<GameManager>(); //get gamemanager component
+
+        if (GM.GameOnGoing) //if game is not paused let the key move
+        {
+            ActiveKey(); //move
+        }
+
+        if (Input.GetKeyDown(KeyCode.P)) //pause game
+        {
+            GM.GameOnGoing = false;
+            Option.SetActive(true);
+        }
+
+        if (Input.GetKeyDown(KeyCode.T)) //resume game
+        {
+            Option.SetActive(false);
+            GameStartCount();
+        }
     }
 
     public void ActiveKey()
@@ -34,20 +54,16 @@ public class PlayerControl : MonoBehaviour
             playerPosition += Vector3.right * moveSpeed * Time.deltaTime;
             rigid.MovePosition(playerPosition);
         }
-        
-        if (Input.GetKeyDown(KeyCode.P))
-        {
-            //pause or resume game
-        }
-
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            //tutorial key
-        }
     }
-    
+
     public Vector3 ClampPosition(Vector3 position)
     {
         return new Vector3(Mathf.Clamp(position.x, -moveLimit.x, moveLimit.x), -3f, 0); //player position always set on x = moveLimit, y = -3, z = 0
+    }
+
+    public void GameStartCount()
+    {
+        //add count down 3
+        GM.GameOnGoing = true;
     }
 }
