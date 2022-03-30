0b14b2c6551e4d06367d1d1ad87de3c098faf64a
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-30 17:58:35 -0500

    FINISHED
    DO NOT EDIT

--- Codes/ProduceManager.cs
+++ Codes/ProduceManager.cs
@@ -1,20 +1,22 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class ProduceManager : MonoBehaviour
 {
-    public GameObject[] Product = new GameObject[7]; //set product
+    public GameObject[] Product = new GameObject[7]; //set products
     public GameObject ProductGroup; //get product group
     public GameObject GameManager; //get gamemanager
+
     public float ProduceTime = 3f; //set produce time
-    bool produceB = true; //determine rather produce item
+    public float dropSpeedMin = 10.0f; //set minimum drop speed of object
+    public float dropSpeedMax = 25.0f; //set maximum drop speed of object
+    bool produceB = true; //determine rather produce item or not
 
     void Update()
     {
-        if (produceB && GameManager.GetComponent<GameManager>().GameOnGoing && GameManager.GetComponent<GameManager>().NotAlive == false)
+        if (produceB && GameManager.GetComponent<GameManager>().GameOnGoing && GameManager.GetComponent<GameManager>().NotAlive == false) //if game is on & player is alive & allow to produce
         {
-            produceB = false; //set false
+            produceB = false; //prevent coroutine to repeat every single frame
             StartCoroutine("Produce"); //start produce coroutine
         }
     }
@@ -23,8 +25,8 @@ public class ProduceManager : MonoBehaviour
     {
         for (int i = 0; i <= 10; i++) //repeat 9 times
         {
-            int ran = Random.Range(0, 7); //get random object number
-            ProduceP(ran); //product produce
+            int ran = Random.Range(0, 7); //get random product to create
+            ProduceP(ran); //produce product
         }
         yield return new WaitForSeconds(ProduceTime);  //wait for produce time
         produceB = true; //set true to restart the coroutine
@@ -35,7 +37,7 @@ public class ProduceManager : MonoBehaviour
         GameObject ProduceP = Instantiate(Product[ran]) as GameObject; //create product
         ProduceP.transform.parent = ProductGroup.transform; //get product location
         ProduceP.transform.position = new Vector3(Random.Range(-8.0f, 8.0f), Random.Range(5.0f, 10.0f), 0); //change product x-axis & y-axis location randomly
-        ProduceP.GetComponent<DropBehavior>().objectSpeed = Random.Range(10.0f, 25.0f); //set ramdom drop speed on dropbehavior script
+        ProduceP.GetComponent<DropBehavior>().objectSpeed = Random.Range(dropSpeedMin, dropSpeedMax); //set ramdom drop speed on dropbehavior script
         ProduceP.GetComponent<DropBehavior>().DropT = true; //let them drop
     }
 }
