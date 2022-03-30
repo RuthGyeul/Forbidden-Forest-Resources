bf8609d92c8c065f3e1bfae27b4f869bac555db1
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-30 17:41:29 -0500

    FINISHED
    DO NOT EDIT

--- Codes/LoadingManager.cs
+++ Codes/LoadingManager.cs
@@ -1,5 +1,4 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
@@ -7,42 +6,38 @@ using UnityEngine.SceneManagement;
 public class LoadingManager : MonoBehaviour
 {
     [SerializeField]
-    Image loadingBar; //get lading bar
+    Image loadingBar; //get loading bar
 
     private void Start()
     {
-        loadingBar.fillAmount = 0; //set loadingbar to 0%
-        StartCoroutine(LoadAsyncScene()); //start loading coroutine
+        loadingBar.fillAmount = 0; //set loadingbar to 0
+        StartCoroutine(ReadyScene()); //start load scene coroutine
     }
 
-    public static void LoadScene(string sceneName)
-    {
-        SceneManager.LoadScene("LaodingScene"); //set loading scene
-    }
-    IEnumerator LoadAsyncScene()
+    IEnumerator ReadyScene()
     {
         yield return null;
-        AsyncOperation asyncScene = SceneManager.LoadSceneAsync("GameScene"); //set scene to change
-        asyncScene.allowSceneActivation = false;
+        AsyncOperation asyncScene = SceneManager.LoadSceneAsync("GameScene"); //call game scene
+        asyncScene.allowSceneActivation = false; //don't change scene yet
         float timeC = 0;
-        while (!asyncScene.isDone)
+        while (!asyncScene.isDone) //if loading is not done
         {
             yield return null;
-            timeC += Time.deltaTime/5; //set time dutration
+            timeC += Time.deltaTime/4; //set time dutration
             if (asyncScene.progress >= 0.9f) //if progress is 90%
             {
-                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, 1, timeC); //limit
-                if (loadingBar.fillAmount == 1.0f) //if bar is filled
+                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, 1, timeC); //give limit
+                if (loadingBar.fillAmount == 1.0f) //if bar is 100% filled
                 {
                     asyncScene.allowSceneActivation = true; //change scene
                 }
             } else {
-                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, asyncScene.progress, timeC); //set fill limit
-                if (loadingBar.fillAmount >= asyncScene.progress) //timeout
+                loadingBar.fillAmount = Mathf.Lerp(loadingBar.fillAmount, asyncScene.progress, timeC); //fill bar as progress goes
+                if (loadingBar.fillAmount >= asyncScene.progress) //when timeout
                 {
-                    timeC = 0f;
+                    timeC = 0f; //reset time
                 }
-            } 
+            }
         }
     }
 }
