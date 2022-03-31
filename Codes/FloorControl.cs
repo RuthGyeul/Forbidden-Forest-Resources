88a967940cb8ecc1de12bd0fc4fb3f371dd6ed76
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-31 10:25:28 -0500

    Update FloorControl.cs

--- Codes/FloorControl.cs
+++ Codes/FloorControl.cs
@@ -7,6 +7,11 @@ public class FloorControl : MonoBehaviour
 {
     public GameObject GameManager;
     
+    public GameObject[] GrassObj = new GameObject[7];
+    public GameObject[] BurnGrassObj = new GameObject[7];
+    public GameObject[] LavaObj = new GameObject[7];
+    
+    public GameObject Grass;
     public GameObject Grass01;
     public GameObject Grass02;
     public GameObject Grass03;
@@ -15,6 +20,16 @@ public class FloorControl : MonoBehaviour
     public GameObject Grass06;
     public GameObject Grass07;
     
+    public GameObject BurnGrass;
+    public GameObject BurnGrass01;
+    public GameObject BurnGrass02;
+    public GameObject BurnGrass03;
+    public GameObject BurnGrass04;
+    public GameObject BurnGrass05;
+    public GameObject BurnGrass06;
+    public GameObject BurnGrass07;
+    
+    public GameObject Lava;
     public GameObject Lava01;
     public GameObject Lava02;
     public GameObject Lava03;
@@ -23,11 +38,57 @@ public class FloorControl : MonoBehaviour
     public GameObject Lava06;
     public GameObject Lava07;
     
+    public bool CreateSinkhole = false;
+    public bool ImOnLava = false;
+    public float LavaDuration = 5.0f;
+    
+    GameManager GM;
+    
     void Start()
     {
+        GM = GameManager.GetComponent<GameManager>();
+        Lava.SetActive(false);
+        BurnGrass.SetActive(true);
+        Grass.SetActive(true);
     }
     
     void Update()
     {
+        if (GM.GameOnGoing && GM.NotAlive == false)
+        {
+            if (ImOnLava)
+            {
+                ImOnLava = false;
+                StopCoroutine("Sinkhole");
+                BurnGrass.SetActive(true);
+                Grass.SetActive(true);
+                Lava.SetActive(false);
+                CreateSinkhole = true;
+            }
+            
+            if (CreateSinkhole)
+            {
+                CreateSinkhole = false;
+                StartCoroutine("Sinkhole");
+            }
+        }
+    }
+    
+    IEnumerator Sinkhole()
+    {
+        int ran = Random.Range(0, 7);
+        
+        for (int i = 0; i <= 10; i++)
+        {
+            GrassObj[ran].SetActive(false);
+            BurnGrassObj[ran].SetActive(true);
+            GrassObj[ran].SetActive(true);
+            BurnGrassObj[ran].SetActive(false);
+        }
+        GrassObj[ran].SetActive(false);
+        LavaObj[ran].SetActive(true);
+        BurnGrassObj[ran].SetActive(false);
+        yield return new WaitForSeconds(LavaDuration);
+        CreateSinkhole = true;
     }
 }
