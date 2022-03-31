c39d04ee87ce7d52e892a4c3f19f272df76b9fa5
R. Gyeul <72477746+RuthGyeul@users.noreply.github.com>
2022-03-31 09:57:42 -0500

    Create FloorControl.cs

---
+++ Codes/FloorControl.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FloorControl : MonoBehaviour
+{
+    public GameObject GameManager;
+    
+    public GameObject Grass01;
+    public GameObject Grass02;
+    public GameObject Grass03;
+    public GameObject Grass04;
+    public GameObject Grass05;
+    public GameObject Grass06;
+    public GameObject Grass07;
+    
+    public GameObject Lava01;
+    public GameObject Lava02;
+    public GameObject Lava03;
+    public GameObject Lava04;
+    public GameObject Lava05;
+    public GameObject Lava06;
+    public GameObject Lava07;
+    
+    void Start()
+    {
+    }
+    
+    void Update()
+    {
+    }
+}
